37e331810e81d7d274e320fd50e96e5e2f343855
AlwaysAwake <tellmewhy07@gmail.com>
2015-10-10 18:23:53 +0900

    Set indent space to 2

--- app.js
+++ app.js
@@ -18,13 +18,13 @@ var ENVIRONMENT = app.get('env');
 
 var config_mongodb = config[DATABASE][ENVIRONMENT];
 mongoose.connect(config_mongodb.uri, {
-    db: {native_parser: true},
-    server: {poolSize: config_mongodb.poolSize},
-    user: config_mongodb.username,
-    pass: config_mongodb.password
+  db: {native_parser: true},
+  server: {poolSize: config_mongodb.poolSize},
+  user: config_mongodb.username,
+  pass: config_mongodb.password
 });
 mongoose.connection.on('open', function () {
-    console.log('Connection opened to mongodb!');
+  console.log('Connection opened to mongodb!');
 });
 
 // view engine setup
@@ -35,7 +35,7 @@ app.set('view engine', 'jade');
 app.use(favicon(__dirname + '/public/images/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -43,10 +43,10 @@ app.use('/', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handlers
@@ -54,23 +54,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
 
