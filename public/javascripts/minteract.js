d93efe1e16dbd2ed0973a60c2e73a2893809753e
One June Kang <onejunekang@gmail.com>
2015-10-10 18:42:06 +0900

    gogo

--- public/javascripts/minteract.js
+++ public/javascripts/minteract.js
@@ -4,17 +4,17 @@ $(document).ready(function(){
       var startchange = $('.secondmain');
       var offset = startchange.offset();
 
-      if(scroll_start > offset.top-90) {
-          $('#ologo').attr('src', '/images/nl.png');
+      // if(scroll_start > 100){
+      //     $('header').css('opacity', '0');
+
+    if( scroll_start > offset.top-90) {
+          $('#ologo').attr('src', '/images/bl.png');
           $('header').css('background-color', '#B1C5FF');
           $('header').css('position', 'fixed');
-
           $('header').animate({
             opacity : 0.8
-          }, 1000);
-       } else if( scroll_start < offset.top-90 && scroll_start > 100){
-          $('header').css('opacity', '0');
-       } else {
+          }, 500);
+       }else {
           $('#ologo').attr('src', '/images/wl.png');
           $('header').css('background-color', 'transparent');
           $('header').css('position', 'absolute');
