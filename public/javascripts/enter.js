2ec6d86053e28b8ce59f1984ecd707bdc7c4bdec
One June Kang <onejunekang@gmail.com>
2015-10-10 21:31:23 +0900

    sibal

--- public/javascripts/enter.js
+++ public/javascripts/enter.js
@@ -1,20 +1,22 @@
 
 $(document).ready(function () {
-$('#joinCall').on('click', function(){
-  $.ajax({
-    method: "POST",
-    url: "/signup",
-    data: {id: $('#id').val, pwd: $('#pwd').val(), pmail: $('#pmail').val(), snum: $('#snum').val(), sname: $('#sname').val() } 
-  }).done(function(msg){
+  $('#joinCall').on('click', function(){
+      $.ajax({
+        method: "POST",
+        url: "/signup",
+        data: {id: $('#id').val, pwd: $('#pwd').val(), pmail: $('#pmail').val(), snum: $('#snum').val(), sname: $('#sname').val() } 
+      }).done(function(msg){
+          console.log(msg);
+        });
+    });
 
-  });
-$('#loginCall').on('click', function(){
-  $.ajax({
-    method: "POST",
-    url: "/signin",
-    data: {id: $('#id').val, pwd: $('#pwd').val() } 
-  }).done(function(msg){
-
-  });
-})
-}
+  $('#loginCall').on('click', function(){
+      $.ajax({
+        method: "POST",
+        url: "/signin",
+        data: {id: $('#id').val, pwd: $('#pwd').val()} 
+      }).done(function(msg){
+          console.log(msg);
+        });
+    });
+});
