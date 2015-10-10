160475621dd4f5982c9934c04f2065c80b93a2b2
lsywind3 <lsywind3@gmail.com>
2015-10-10 18:07:38 +0900

    signup module 추가

--- routes/index.js
+++ routes/index.js
@@ -22,11 +22,40 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Respect.ly' });
 });
 
-router.post('/signup', function(req, res, next) {
-    res.render('index', { title: 'Respect.ly' });
+router.get('/signup', function(req, res, next) {
+    var user = req.param('user');
+    var email = user.email;
+
+    var query = _$usr
+        .findOne({'profile.email': email})
+        .select('profile')
+        .sort('profile.email')
+        .lean(true);
+
+    query.exec(function (err, docUsr) {
+        if (err) return next(err);
+
+        if (docUsr) {
+            res.header('Content-Type', 'application/json');
+            res.header('content-length', Buffer.byteLength(JSON.stringify({"message": "Email exists."})));
+            res.end(JSON.stringify({"message": "Email exists."}));
+        } else {
+            var usrGen = generateUser(user);
+
+            _$usr.create(usrGen, function (err, docUsr) {
+                if (err) return next();
+
+                var name = docUsr.profile.name;
+
+                res.header('Content-Type', 'application/json');
+                res.header('content-length', Buffer.byteLength(JSON.stringify({ "name": name })));
+                res.end(JSON.stringify({ "name": name }));
+            });
+        }
+    });
 });
 
-router.post('/signin', function(req, res, next) {
+router.get('/signin', function(req, res, next) {
     res.render('index', { title: 'Respect.ly' });
 });
 
