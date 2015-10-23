0c8bfe3492ecec32bd41cd851272c377cb4f9307
One June Kang <onejunekang@gmail.com>
2015-10-23 21:53:36 +0900

    code refactor

--- public/javascripts/minteract.js
+++ public/javascripts/minteract.js
@@ -1,36 +1,25 @@
 $(document).ready(function () {
-  $(document).scroll(function () {
-    scroll_start = $(this).scrollTop();
+  $(window).scroll(function () {
+    scroll_start = $(window).scrollTop();
     var startchange = $('.secondmain');
     var offset = startchange.offset();
 
-    if (scroll_start > offset.top - 250) {
-      $('#ologo').attr('src', '/images/vl.png');
-      $('header').css('background-color', '#03ae6d');
-      $('header').css('background-image', 'linear-gradient(to left, #07b15f 0, #00ac92 50%, #00849e 100%)');
-      $('header').css('position', 'fixed');
-      $('header').css('opacity', '0.8');
-
-      $('header').css('height', '60px');
-      $('.mainnav').css('height', '60px');
-      $('.mainnav a').css('padding', '18px 20px');
-      $('ul').css('height', '60px');
-      $('.site-logo').css('padding', '0px');
-      $('a.logo').css('padding', '0px');
-
-    } else {
+    if (scroll_start < offset.top - 250) { // top 
       $('#ologo').attr('src', '/images/wl.png');
-      $('header').css('background-color', 'transparent');
-      $('header').css('background-image', 'none');
-      $('header').css('position', 'absolute');
-      $('header').css('opacity', '1');
-
-      $('header').css('height', '80px');
       $('.mainnav').css('height', '80px');
       $('.mainnav a').css('padding', '28px 20px');
       $('ul').css('height', '80px');
       $('.site-logo').css('padding', '11px');
       $('a.logo').css('padding', '0px');
+      $('header').fadeOut().removeClass('fixedheader').addClass('absoluteheader');
+    } else { // bottom
+      $('#ologo').attr('src', '/images/vl.png');
+      $('.mainnav').css('height', '60px');
+      $('.mainnav a').css('padding', '18px 20px');
+      $('ul').css('height', '60px');
+      $('.site-logo').css('padding', '0px');
+      $('a.logo').css('padding', '0px');
+      $('header').removeClass('absoluteheader').addClass('fixedheader').fadeIn();
     }
   });
 });
