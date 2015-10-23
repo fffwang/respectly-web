44ae86520740d15fd9026a085d0e3d83010bec56
One June Kang <onejunekang@gmail.com>
2015-10-23 23:18:39 +0900

    refacotring nav bar

--- public/javascripts/minteract.js
+++ public/javascripts/minteract.js
@@ -1,17 +1,18 @@
-$(document).ready(function () {
+$(function () {
   $(window).scroll(function () {
     scroll_start = $(window).scrollTop();
     var startchange = $('.secondmain');
     var offset = startchange.offset();
-
-    if (scroll_start < offset.top - 250) { // top 
+    if (scroll_start < offset.top - 650) { // top 
       $('#ologo').attr('src', '/images/wl.png');
       $('.mainnav').css('height', '80px');
       $('.mainnav a').css('padding', '28px 20px');
       $('ul').css('height', '80px');
       $('.site-logo').css('padding', '11px');
       $('a.logo').css('padding', '0px');
-      $('header').fadeOut().removeClass('fixedheader').addClass('absoluteheader');
+      $('header').removeClass('fixedheader').addClass('absoluteheader').fadeIn();
+    } else if (scroll_start > offset.top - 650 && scroll_start < offset.top - 250){
+      $('header').fadeOut();
     } else { // bottom
       $('#ologo').attr('src', '/images/vl.png');
       $('.mainnav').css('height', '60px');
@@ -21,5 +22,6 @@ $(document).ready(function () {
       $('a.logo').css('padding', '0px');
       $('header').removeClass('absoluteheader').addClass('fixedheader').fadeIn();
     }
+    
   });
 });
