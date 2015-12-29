fd114c27b658d70c543fd77983370da8f49d0c91
AlwaysAwake <tellmewhy07@gmail.com>
2015-12-29 12:09:41 +0900

    changed query of mongodb, created delete handler of project join/support

--- routes/projects.js
+++ routes/projects.js
@@ -13,13 +13,37 @@ router.post('/join', function(req, res, next) {
   if (!pid || !uid) return next(new Error("No pid or uid sent"));
 
   _$pjt.findByIdAndUpdate(pid,
-    {$push: {"_collaborators": uid}},
+    {$addToSet: {"_collaborators": uid}},
     {safe: true, upsert: true},
     function(err, docPjt) {
       if (err || !docPjt) return next(err || new Error("No projects matching that id"));
 
       _$usr.findByIdAndUpdate(uid,
-        {$push: {"_projects": pid}},
+        {$addToSet: {"_projects": pid}},
+        {safe: true, upsert: true},
+        function(err, docUsr) {
+          if (err || !docUsr) return next(err || new Error("No users matching that id"));
+
+          return res.status(200).json({ message: msg.success.GENERAL });
+        }
+      );
+    }
+  );
+});
+
+router.delete('/join', function(req, res, next) {
+  var uid = req.body.uid;
+  var pid = req.body.pid;
+  if (!pid || !uid) return next(new Error("No pid or uid sent"));
+
+  _$pjt.findByIdAndUpdate(pid,
+    {$pull: {"_collaborators": uid}},
+    {safe: true, upsert: true},
+    function(err, docPjt) {
+      if (err || !docPjt) return next(err || new Error("No projects matching that id"));
+
+      _$usr.findByIdAndUpdate(uid,
+        {$pull: {"_projects": pid}},
         {safe: true, upsert: true},
         function(err, docUsr) {
           if (err || !docUsr) return next(err || new Error("No users matching that id"));
@@ -37,13 +61,37 @@ router.post('/support', function(req, res, next) {
   if (!pid || !uid) return next(new Error("No pid or uid sent"));
 
   _$pjt.findByIdAndUpdate(pid,
-    {$push: {"_supporters": uid}},
+    {$addToSet: {"_supporters": uid}},
     {safe: true, upsert: true},
     function(err, docPjt) {
       if (err || !docPjt) return next(err || new Error("No projects matching that id"));
 
       _$usr.findByIdAndUpdate(uid,
-        {$push: {"_supportedProjects": pid}},
+        {$addToSet: {"_supportedProjects": pid}},
+        {safe: true, upsert: true},
+        function(err, docUsr) {
+          if (err || !docUsr) return next(err || new Error("No users matching that id"));
+
+          return res.status(200).json({ message: msg.success.GENERAL });
+        }
+      );
+    }
+  );
+});
+
+router.delete('/support', function(req, res, next) {
+  var uid = req.body.uid;
+  var pid = req.body.pid;
+  if (!pid || !uid) return next(new Error("No pid or uid sent"));
+
+  _$pjt.findByIdAndUpdate(pid,
+    {$pull: {"_supporters": uid}},
+    {safe: true, upsert: true},
+    function(err, docPjt) {
+      if (err || !docPjt) return next(err || new Error("No projects matching that id"));
+
+      _$usr.findByIdAndUpdate(uid,
+        {$pull: {"_supportedProjects": pid}},
         {safe: true, upsert: true},
         function(err, docUsr) {
           if (err || !docUsr) return next(err || new Error("No users matching that id"));
